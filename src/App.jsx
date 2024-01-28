b11facb861109f4a14c18c3c13aa508668f0e091
Charly <123911937+CharlyKrDev@users.noreply.github.com>
2024-01-28 05:05:47 +0100

    Primera Pre-Entrega v1.0

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './styles/App.css'
+import { Navbar } from './components/Navbar'
+
+export const App = () => {
+  return (
+      <>
+    <Navbar/>
+      </>
+  )
+  }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React, { useState} from 'react';
+export const Navbar = () => {
+
+    const [iconName, setIconName] = useState('menu');
+    const [menuOpen, setMenuOpen] = useState(false);
+
+
+    const onToggleMenu = () => {
+        const newIconName = iconName === 'menu' ? 'close' : 'menu';
+        setIconName(newIconName);
+        console.log(newIconName);
+        setMenuOpen(!menuOpen);
+
+    };
+    const menuClass = `duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 ${menuOpen ? 'top-[7%]' : 'top-[-100%]'} md:w-auto flex items-center px-5 w-full`;
+
+    return (
+
+        <header className='bg-white font-bold text-black'>
+
+            <nav className='flex justify-between items-center w-[92%] mx-auto'>
+                <a href=""><img className='w-32' src="../public/eCommerce.svg" alt="logo" /></a>
+                <div className={menuClass}>
+                    <ul className='flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8'>
+                        <li><a className='hover:text-gray-500' href="">Tienda</a></li>
+                        <li><a className='hover:text-gray-500' href="">Quienes somos?</a></li>
+                        <li><a className='hover:text-gray-500' href="">Contacto</a></li>
+                        <li><a className='hover:text-gray-500' href="">Vemos</a></li>
+                    </ul>
+                </div>
+                <div className='flex items-center gap-6'>
+                    <button className='bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]'>
+                        Ingresar
+                    </button>
+
+                    <ion-icon onClick={onToggleMenu} name={iconName} class='cursor-pointer text-3xl md:hidden '></ion-icon>
+
+
+                </div>
+
+            </nav>
+
+        </header>
+
+
+    )
+} 
