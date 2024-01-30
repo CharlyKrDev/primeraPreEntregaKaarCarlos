962680f43c654012c7e6304fa2e46449b726ae64
Charly <123911937+CharlyKrDev@users.noreply.github.com>
2024-01-30 21:42:07 +0100

    Primera Pre-Entrega v1.2

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -4,7 +4,7 @@ export const CartWidget = () => {
   return (
 
     <a className='flex gap-1' href="#">
-      <img className='w-10 invert' src="../src/assets/carrito.png" alt="carrito" />
+      <img className='w-10 invert' src="./src/assets/carrito.png" alt="carrito" />
       <span className='bg-blue-700 text-white rounded-lg h-6 px-2 mr-5'>0
       </span>
       </a>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -7,7 +7,7 @@ export const ItemListContainer = () => {
         <section className='flex flex-col text-white items-center justify-center bg-[#242424] min-h-[30vh] box-content w-auto h-auto border-4'>CARRUSEL</section>
         <section className='md:flex gap-10 justify-center max-h-[70vh]'>
         <article className='flex flex-col w-1/3 md:m-4 m-auto'><h1 className='font-bold'><WelcomeTyping/></h1><p className='m-auto text-lg'>En nuestra web no solo vas a encontrar los <strong>juegos</strong> más actuales del mercado, sino que también vas a poder ser parte de una de las comunidades <strong>gamers</strong> más importantes!</p></article>
-        <div className='flex flex-col w-auto items-center '><img className='bg-white max-h-[60vh]'src="/public/portada.jpg" alt="portada" /></div>
+        <div className='flex flex-col w-auto items-center '><img className='bg-white max-h-[60vh]'src="./src/assets/portada.jpg" alt="portada" /></div>
         </section>
         
     </main>
