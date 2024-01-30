fc260c6cc255b89a9125ed4170c910b9bb82b305
Charly <123911937+CharlyKrDev@users.noreply.github.com>
2024-01-30 16:14:43 +0100

    Primera Pre-Entrega v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './styles/App.css'
 import { Navbar } from './components/Navbar'
 import { ItemListContainer } from './components/ItemListContainer'
+
 export const App = () => {
   return (
       <>
     <Navbar/>
     <ItemListContainer/>
+   
 
       </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState} from 'react';
 import { CartWidget } from './CartWidget';
+import { SearchInput } from './SearchInput'
+
 export const Navbar = () => {
 
     const [iconName, setIconName] = useState('menu');
@@ -13,25 +15,26 @@ export const Navbar = () => {
         setMenuOpen(!menuOpen);
 
     };
-    const menuClass = `duration-500 md:static absolute bg-black opacity-80 md:min-h-fit min-h-[60vh] left-0 ${menuOpen ? 'top-[7%]' : 'top-[-100%]'} md:w-auto flex items-center px-5 w-full`;
+    const menuClass = `duration-500 md:static absolute bg-black opacity-80 md:min-h-fit min-h-[60vh] left-0 ${menuOpen ? 'top-[8%]' : 'top-[-100%]'} md:w-auto flex items-center px-5 w-full`;
 
     return (
 
         <header className='bg-black font-bold text-white'>
 
             <nav className='flex justify-between items-center w-[99%] mx-auto'>
-                <a className='flex gap-2 items-center justify-center ml-3' href="#"><img className='w-24 mt-0.5 invert' src="../public/eCommerce.svg" alt="logo" /><img className='w-16' src="../public/logo.jpg" alt="logo" /></a>
+                <a className='flex gap-1 items-center justify-center ml-5' href="#"><img className='w-16 mt-0.5 invert' src="/public/eCommerce.svg" alt="logo" /><img className='w-16 mr-2' src="/public/logo.jpg" alt="logo" /></a>
                 <div className={menuClass}>
                     <ul className='flex md:flex-row flex-col md:items-center md:gap-[2vw] gap-8'>
                         <li><a className='hover:text-gray-500' href="#">Tienda</a></li>
                         <li><a className='hover:text-gray-500' href="#">Quienes somos?</a></li>
                         <li><a className='hover:text-gray-500' href="#">Comunidad</a></li>
                         <li><a className='hover:text-gray-500' href="#">Contacto</a></li>
+                        <li><SearchInput/></li>
                     </ul>
                 </div>
                 <CartWidget/>
                 <div className='flex items-center gap-6'>
-                <button className='bg-[#a6c1ee] text-white px-2 py-2 rounded-full hover:bg-[#87acec] mr-2'>
+                <button className='text-white px-2 py-2 bg-blue-700 hover:bg-blue-800 rounded-lg mr-2'>
                         Ingresar
                     </button>
 
