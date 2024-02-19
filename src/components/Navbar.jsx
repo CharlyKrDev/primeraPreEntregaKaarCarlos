c1741e4471d203b87de5aead1b801bd635d17276
Charly <123911937+CharlyKrDev@users.noreply.github.com>
2024-02-19 20:01:45 +0100

    rectificacion de footer en landing, contador de productos, routers, page404, categorias, y estilos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ export const Navbar = () => {
     setMenuOpen(!menuOpen);
   };
   const menuClass = `duration-500 md:static absolute bg-black opacity-80 md:min-h-fit min-h-[60vh] left-0 ${
-    menuOpen ? "top-[10%]" : "top-[-100%]"
+    menuOpen ? "top-[8%]" : "top-[-100%]"
   } md:w-auto flex items-center px-5 w-full`;
   const menuGeneral = `text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center`;
 
