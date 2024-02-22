eca2509cc640d7f15203c593aabb6afefca274e9
Charly <123911937+CharlyKrDev@users.noreply.github.com>
2024-02-22 02:34:12 +0100

    Listo para 2da preEntrega

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,4 +1,4 @@
-
+import WorkInProgress from "../../public/img/work-in-progress.jpg"
 
 export const Checkout = () => {
 
@@ -6,7 +6,7 @@ export const Checkout = () => {
 
         <div className="flex flex-col min-h-[81.2vh]">
         <h2 className="m-auto pr-8 mt-20 mb-20">PRÓXIMAMENTE SE VIENE EL CARRITO</h2>
-        <img className='max-h-[400px] max-w-[400px] m-auto invert' src="../../public/img/work-in-progress.jpg" alt="trabajoEnProgreso" />
+        <img className='max-h-[400px] max-w-[400px] m-auto invert' src={WorkInProgress} alt="trabajoEnProgreso" />
         </div>
     )
 }
