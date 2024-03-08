d5612c6c248397e6301270cf46ea41241f157529
Charly <123911937+CharlyKrDev@users.noreply.github.com>
2024-03-08 15:31:15 +0100

    Version Final 0.0.1

--- src/hooks/useFavorito.js
+++ src/hooks/useFavorito.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
 export const useFavorito = (icono = "heart-outline") => {
-  const [iconNombre, useFavorito] = useState(icono);
+  const [iconNombre, setIconNombre] = useState(icono);
 
   const agregarFavorito = () => {
     const nuevoFavorito =
       iconNombre === "heart-outline" ? "heart" : "heart-outline";
     console.log(nuevoFavorito);
-    useFavorito(nuevoFavorito);
+    setIconNombre(nuevoFavorito);
   };
 
   return { iconNombre, agregarFavorito };
